0ccc37ad99a8de4c7dda19b757032849e1b0aebe
ayushraj706 <ayushrajayushhh@gmail.com>
2026-02-10 23:16:58 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // अगर undici का एरर आए तो यह लाइन उसे शांत कर देगी
-  transpilePackages: ['undici', 'firebase'],
+  // यह लाइन उस undici वाले एरर को ठीक करेगी
+  transpilePackages: ['undici', 'firebase', '@firebase/auth'],
+  
+  // अगर फ्यूचर में इमेज का एरर आए तो यह काम आएगा
   images: {
-    domains: ['lh3.googleusercontent.com'], // Google Profile Pic के लिए
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
+    ],
   },
 };
 
 module.exports = nextConfig;
-
